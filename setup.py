fb418dad8a529365daec147627595c8eba869ccc
arytontediarjo <35942857+arytontediarjo@users.noreply.github.com>
2020-10-09 10:36:32 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PDKitRotationFeatures',
-    version='0.0.1',
+    version='0.0.2',
     description='package wrapper for gait feature pipeline',
     author='Aryton Tediarjo and Larsson Omberg',
     author_email='aryton.tediarjo@sagebase.org',
